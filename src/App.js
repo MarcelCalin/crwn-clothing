cc32ccf656e71ec719748eebcf9b3f368f8a7ae6
MarcelCalin <marcel.calin@gmail.com>
2022-06-13 14:55:31 +0300

    withrouter added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component.jsx";
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from "react";
-import "./index.css";
-import App from "./App";
-import { createRoot } from "react-dom/client";
-// import ReactDOM from "react-dom";
-// ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+import './index.css';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+// root.render(<App tab="home" />);
+root.render(
+  <BrowserRouter>
+    <App tab='home' />
+  </BrowserRouter>
+);
