300370c6858d0a43c6f5ef1a1e928646619ec3d4
MarcelCalin <marcel.calin@gmail.com>
2022-06-17 17:27:46 +0300

    minor correction update to form component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -56,7 +56,7 @@ class SignUp extends React.Component {
   render() {
     const { displayName, password, confirmPassword, email } = this.state;
     return (
-      <div className='sign-ip'>
+      <div className='sign-up'>
         <h2 className='title'>I do not have an acount</h2>
         <span> Sign up with your email and password</span>
         <form className='sign-up-form' onSubmit={this.handleSubmit}>
